0ae2a5bbe4b13f0248d9cae6b964aef41b6da93b
Tom <thrclark@indiana.edu>
2016-07-21 09:40:10 -0400

    cleanup

--- php/includes/styles.php
+++ php/includes/styles.php
@@ -11,24 +11,13 @@
 <link href="../less/brand.css" rel="stylesheet" type="text/css">
 <link href="../vendor/bootstrap3-editable/css/bootstrap-editable.css" rel="stylesheet" type="text/css">
 <link href="../app/vendor/js/colorbox/colorbox.css" rel="stylesheet" type="text/css">
-
 <link href="../app/vendor/js/expandgrid/css/component.css" rel="stylesheet" type="text/css">
-
-
-
-
-
-
 
 <!--<link rel="stylesheet" type="text/css" href="../app/vendor/js/slick/slick.css"/>
 <link rel="stylesheet" type="text/css" href="../app/vendor/js/slick/slick-theme.css"/>-->
 
 <link rel="stylesheet" type="text/css" href="../app/vendor/js/lightSlider/css/lightslider.css">
-
-
- <link type="text/css" rel="stylesheet" href="../app/vendor/js/lightGallery/css/lightgallery.css" /> 
-
-
+<link type="text/css" rel="stylesheet" href="../app/vendor/js/lightGallery/css/lightgallery.css" />
 
 <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
 <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -38,10 +27,6 @@
     <![endif]-->
 
 <style>
-
-
-
-
 .menuslide-out {
 	right: -227px;
 	visibility: hidden;
@@ -50,142 +35,62 @@
 	right: 0px;
 	visibility: visible;
 }
-
-
-
-
-
-.bottom-container-controls i {
-	margin-left: -3px;
-	margin-right: -3px;
-}
-.label {
-	border-radius: 0;
-	-webkit-border-radius: 0;
-	-moz-border-radius: 0;
-}
-h2 a, h2 a:hover {
-	color: inherit;
-	text-decoration: none;
-}
-.trash {
-	color: rgb(209, 91, 71);
-}
-.flag {
-	color: rgb(248, 148, 6);
-}
-.panel-body {
-	padding: 0px;
-}
-.panel-footer .pagination {
-	margin: 0;
-}
-.panel .glyphicon, .list-group-item .glyphicon {
-	margin-right: 5px;
-}
-.panel-body .radio, .checkbox {
-	display: inline-block;
-	margin: 0px;
-}
-.panel-body input[type=checkbox]:checked + label {
-	text-decoration: line-through;
-	color: rgb(128, 144, 160);
-}
-.list-group-item:hover, a.list-group-item:focus {
-	text-decoration: none;
-	background-color: rgb(245, 245, 245);
-}
-.list-group {
-	margin-bottom: 0px;
-}
-.rating-list {
-	font-size: 12px;
-	color: #727272
-}
-.rating-list .stars {
-	display: inline
-}
-.rating-list .progress {
-	height: 5px;
-	display: inline-flex;
-	width: 150px;
-	margin-bottom: 0px
-}
 </style>
-<style>
-.btn-round-lg {
-	border-radius: 10px;
-}
-.btn-round {
-	border-radius: 17px;
-}
-.btn-round-sm {
-	border-radius: 15px;
-}
-.btn-round-xs {
-	border-radius: 11px;
-	padding-left: 10px;
-	padding-right: 10px;
-}
-</style>
-
-
 <style type="text/css">
-.twitter-typeahead .tt-dropdown-menu .tt-dataset-task h3,
-.twitter-typeahead .tt-dropdown-menu .tt-dataset-task-center h3 {
+.twitter-typeahead .tt-dropdown-menu .tt-dataset-task h3, .twitter-typeahead .tt-dropdown-menu .tt-dataset-task-center h3 {
 	color: #555;
 	padding: 3px 3px 10px 0;
 	margin: 10px 20px 5px 20px;
 	border-bottom: 1px solid #ccc;
-	font-size:18px;
-	    color: #a94442;
+	font-size: 18px;
+	color: #a94442;
 }
-
 .twitter-typeahead .tt-suggestion:hover {
-    color: #fff;
-    background-color: #337ab7;
+	color: #fff;
+	background-color: #337ab7;
 }
-
-
 .twitter-typeahead .tt-suggestion:hover .text-muted {
 	padding-left: 10px;
 	color: #fff;
 }
 </style>
 
-<style>
-.loadthis { display:none}</style>
-
-
-
-
 
 <style>
 
-.tab-content .tab-pane .panel-main .task6 {
-	width: 292px;
-}
-.tab-content .tab-pane .panel-main .task6 .tile-content {
-	width: 203px;
-}
-@media (max-width: 992px) {
-
-.tab-content .tab-pane .panel-main .task6 {
-	width: 341px;
-}
-.tab-content .tab-pane .panel-main .task6 .tile-content {
-	width: 247px;
-}
-
-}
-
-@media (max-width: 767px) {
-
-.tab-content .tab-pane .panel-main .task6 {
-	width: 100%;
-}
-.tab-content .tab-pane .panel-main .task6 .tile-content {
-	width: 100%;
-}
-
-}</style>
+	.global-announcement.color1 .ga-header {
+		background: #786BC5;
+		color: #ffffff;
+		text-shadow: 1px 1px 1px #000;
+	}
+	.global-announcement.color2 .ga-header {
+		background: #E39A41;
+		color: #ffffff;
+		text-shadow: 1px 1px 1px #000;
+	}
+	.global-announcement.color3 .ga-header {
+		background: #BC545F;
+		color: #ffffff;
+		text-shadow: 1px 1px 1px #000;
+	}
+	.global-announcement.color4 .ga-header {
+		background: #00BB9A;
+		color: #ffffff;
+		text-shadow: 1px 1px 1px #000;
+	}
+	.global-announcement.color5 .ga-header {
+		background: #44AADF;
+		color: #ffffff;
+		text-shadow: 1px 1px 1px #000;
+	}
+	
+	.global-announcement.color6 .ga-header {
+		background: #60574d;
+		color: #ffffff;
+		text-shadow: 1px 1px 1px #000;
+	}
+    
+    
+    
+    </style>
+    
