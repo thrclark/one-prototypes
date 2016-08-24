8140668f6f537bcb8d4de04b3414ec14e9b8d8c2
Tom <thrclark@indiana.edu>
2016-08-24 08:59:57 -0400

    adding view with multiple alerts

---
+++ php/includes/alert-multiple.php
@@ -0,0 +1,24 @@
+<div class="alert alert-application" role="alert">
+    <div class="container">
+        <div class="message">
+            <div class="message-visual hidden-xs"> <i class="icon-attention-circled"></i></div>
+            <div class="message-header"> Tornado Watch </div>
+            <div class="message-content">
+                <p> The National Weather Service is reporting a tornado sighting in Monroe County. Please seek shelter immediately. </p>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="alert alert-application" role="alert">
+    <div class="container">
+        <div class="message">
+            <div class="message-visual hidden-xs"> <i class="icon-attention-circled"></i></div>
+            <div class="message-header"> Gas Leak on Campus </div>
+            <div class="message-content">
+                <p> A natural gas line has ruptured on campus, near the north entrance to the Administration Building. Please avoid this area until further notice.</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+
