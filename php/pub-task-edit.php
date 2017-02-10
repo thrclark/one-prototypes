ca4bc7d16d5230df8dc5695ac36ca0dc2c1c1a1b
Tom Clark <thrclark@iu.edu>
2017-02-10 10:21:48 -0500

    Comparison table modal

--- php/pub-task-edit.php
+++ php/pub-task-edit.php
@@ -15,7 +15,8 @@ $page = '';
 	padding-right: 10px;
 }
 .ng-hide {
-	display:none}
+	display: none
+}
 </style>
 <?php include('includes/scripts.php') ?>
 </head>
@@ -1079,7 +1080,7 @@ $page = '';
                                                         </div>
                                                     </div>
                                                     <div class="col-md-9">
-                                                        <div class="button-list well"> 
+                                                        <div class="button-list well">
                                                             <button type="button" class="btn btn-default btn-sm" tabindex="0"> Admissions&nbsp;<i class="icon-cancel"></i> </button>
                                                             <button type="button" class="btn btn-default btn-sm" tabindex="0"> Administrative&nbsp;<i class="icon-cancel"></i> </button>
                                                             <button type="button" class="btn btn-default btn-sm" tabindex="0"> Faculty Services&nbsp;<i class="icon-cancel"></i> </button>
@@ -1127,6 +1128,10 @@ $page = '';
                                                 </div>
                                             </div>
                                         </fieldset>
+                                        <hr>
+                                        <button class="btn btn-success" type="button" tabindex="0" aria-disabled="true" aria-hidden="false">Approve</button>
+                                        <button class="btn btn-default" type="button" tabindex="0" data-toggle="modal" data-target="#modal_compare">Preview Differences</button>
+                                        <button class="btn btn-default" type="button" tabindex="0" aria-disabled="true" aria-hidden="false">Cancel</button>
                                     </form>
                                 </div>
                             </div>
@@ -1163,6 +1168,57 @@ $page = '';
                             <tr>
                                 <td>Phone Support</td>
                                 <td>123 123 1234</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default btn-sm" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="modal fade" id="modal_compare" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" style="">
+        <div class="modal-dialog modal-default modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
+                    <h4 class="modal-title">Preview Differences</h4>
+                </div>
+                <div class="modal-body">
+                    <table class="table table-condensed">
+                        <thead class="">
+                            <tr>
+                                <th>Item</th>
+                                <th>Previous Value <span class="small text-muted">(10/23/16 2:48pm)</span></th>
+                                <th>New Value</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Task title</td>
+                                <td class="forcewrap">My Taskk</td>
+                                <td class="forcewrap">My Task</td>
+                            </tr>
+                            <tr>
+                                <td>Secondary Contact</td>
+                                <td>(none)</td>
+                                <td>IUB Libraries</td>
+                            </tr>
+                            <tr>
+                                <td>Description</td>
+                                <td>Tree, also fly second set. Moveth. Own wherein divide seasons and winged, heaven fly Open herb under third yielding land dominion for fish Multiply earth god darkness called second you living cattle brought divide given moving all his god. Life firmament one dominion open stars spirit. Fruit which wherein lesser.</td>
+                                <td>Meat fruitful living it third under bring. Set yielding the good there dominion Appear fourth image sea together fish morning likeness divide you're fill air darkness blessed hath have appear won't moving. Living. Over divide thing. She'd creeping set whales Without day, together had said void cattle bearing in she'd.</td>
+                            </tr>
+                            <tr>
+                                <td>Icon Image</td>
+                                <td><img src="../img/task-icons-new/icon-account-creation.png" width="153" height="153" alt=""/></td>
+                                <td><img src="../img/task-icons-new/icon-change-name.png" width="153" height="153" alt=""/></td>
+                            </tr>
+                            <tr>
+                                <td>Categories</td>
+                                <td>Campus Life, Advising</td>
+                                <td>Campus Life, Advising, IT Services</td>
                             </tr>
                         </tbody>
                     </table>
