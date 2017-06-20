460202603bf496168efb7433d9f2c11ebbd88dbb
Tom Clark <thrclark@iu.edu>
2017-06-20 15:04:30 -0400

    Schedule release modal

--- php/pub-taskmaint-edit.php
+++ php/pub-taskmaint-edit.php
@@ -56,52 +56,22 @@ $page = '';
             </div>
         </div>
     </div>
-    <div class="modal fade" id="modal_publishdate" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-        <div class="modal-dialog modal-default" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
-                    <h4 class="modal-title">Submit Task for Approval</h4>
-                </div>
-                <div class="modal-body">
-                    <div class="row">
-                        <div class="form-group col-md-12">
-                            <label class="control-label " for="api_account_type"> Publish Date Options </label>
-                            <div class="radio">
-                                <label>
-                                    <input type="radio" name="cars" value="1">
-                                    Upon approval, publish this task immediately. </label>
-                            </div>
-                            <div class="radio">
-                                <label>
-                                    <input type="radio" name="cars" value="2">
-                                    Upon approval, leave this task set to 'Inactive'.</label>
-                            </div>
-                            <div class="radio">
-                                <label>
-                                    <input type="radio" name="cars" value="3">
-                                    Set this task to 'Active' on a specified date (pending approval).</label>
-                            </div>
-                        </div>
-                        <div class="form-group col-md-12 pubdate_widget pubdate_widget3"  style="display:none">
-                            <label for="title" class="control-label">Set Date</label>
-                            <div class="row">
-                                <div class="form-group col-md-6">
-                                    <div class="counter-container">
-                                        <input type="text" name="title" required="" countdown="" class="form-control picktime"  tabindex="0" aria-required="false" aria-invalid="false">
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-default btn-sm" data-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary btn-sm" data-dismiss="modal">Submit Task for Approval</button>
-                </div>
-            </div>
-        </div>
-    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     <div class="modal fade" id="modal_contactinfo" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
         <div class="modal-dialog modal-default" role="document">
             <div class="modal-content">
@@ -190,6 +160,13 @@ $page = '';
             </div>
         </div>
     </div>
+    
+    
+       <?php include('modal/pub-task-publishdate.php') ?>
+       
+       
+       
+       
     <?php include('includes/brand-footer.php') ?>
     <?php include('includes/footer-scripts.php') ?>
     <script>
--- php/pub-taskmaint-edit1.php
+++ php/pub-taskmaint-edit1.php
@@ -1176,7 +1176,7 @@ hr {
                                 </div>
                                 <div class="col-md-4">
                                     <div class="well well-sm" id="page_controls">
-                                        <button type="button" disabled class="btn btn-primary btn-block" id="submitapproval">Submit for Approval...</button>
+                                        <button type="button" disabled class="btn btn-primary btn-block" id="submitapproval" data-toggle="modal" data-target="#modal_publishdate">Submit for Approval...</button>
                                         <button type="button" disabled class="btn btn-default btn-block" id="savedraft">Save as Draft</button>
                                         <button type="button" disabled class="btn btn-default btn-block" id="cancelbutton">Cancel</button>
                                     </div>
@@ -1188,6 +1188,10 @@ hr {
             </div>
         </div>
     </div>
+    
+           <?php include('modal/pub-task-publishdate.php') ?>
+           
+           
     <?php include('includes/brand-footer.php') ?>
     <?php include('includes/footer-scripts.php') ?>
     <script>
